95e2266273115547dd60f167c32441a9cf79c744
asif247 <86229363+asif247@users.noreply.github.com>
2023-04-30 04:33:25 +0600

    logo and left right space added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import logo from '../../../../public/assets/logo.png'
 const Header = () => {
     return (
         <div>
-            <h2>This is header</h2>
+            <div className="text-center">
+                <img src={logo} alt="" />
+                <p className="text-secondary">
+                    <small>Journalism Without Fear or Favour</small>
+                </p>
+            </div>
         </div>
     );
 };
