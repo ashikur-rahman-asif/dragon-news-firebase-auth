c08f752cab73139a4eae960b00079a873f560085
asif247 <86229363+asif247@users.noreply.github.com>
2023-05-01 00:22:46 +0600

    right nav correction

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Button } from "react-bootstrap";
-import { FaGoogle, FaGithub, FaFacebook, FaInstagram,FaTwitter } from "react-icons/fa";
-import ListGroup from 'react-bootstrap/ListGroup';
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
+import ListGroup from "react-bootstrap/ListGroup";
 import QZone from "../QZone/QZone";
-import bg from '../../../../public/assets/bg.png'
-
+import bg from "../../../../public/assets/bg.png";
+import "./RightNav.css";
 const RightNav = () => {
   return (
     <div>
@@ -19,15 +25,29 @@ const RightNav = () => {
       <div className="mt-3">
         <h4>Find us on</h4>
         <ListGroup>
-          <ListGroup.Item><FaFacebook /> Facebook</ListGroup.Item>
-          <ListGroup.Item><FaTwitter /> Twitter</ListGroup.Item>
-          <ListGroup.Item><FaInstagram /> Instagram</ListGroup.Item>
+          <ListGroup.Item>
+            <FaFacebook /> Facebook
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaTwitter /> Twitter
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <FaInstagram /> Instagram
+          </ListGroup.Item>
         </ListGroup>
-          </div>
-          <QZone></QZone>
-          <div>
-              <img className="w-100 mt-3" src={bg} alt="" />
+      </div>
+      <QZone></QZone>
+      <div className="position-relative">
+        <img className="w-100 mt-3" src={bg} alt="" />
+        <div className="position-absolute top-0 text-white text-center mt-5">
+          <h1>Create an Amazing Newspaper</h1>
+          <p className="mt-5">
+            Discover thousands of options, easy to customize layouts, one-click
+            to import demo and much more.
+          </p>
+          <Button className="p-3 mt-5" variant="danger">Learn More</Button>
         </div>
+      </div>
     </div>
   );
 };
