4dfd2ccb8c86d34692c187afe23390752645f460
asif247 <86229363+asif247@users.noreply.github.com>
2023-04-30 19:33:46 +0600

    navbvar done

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -3,6 +3,9 @@ import logo from "../../../../public/assets/logo.png";
 import moment from "moment";
 import { Button, Container } from "react-bootstrap";
 import Marquee from "react-fast-marquee";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 const Header = () => {
   return (
     <Container className="mt-3">
@@ -20,6 +23,24 @@ const Header = () => {
           text.
         </Marquee>
       </div>
+      <Navbar className="mt-3" collapseOnSelect expand="lg" bg="light" variant="light">
+        <Container>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mx-auto">
+              <Nav.Link href="#features">Home</Nav.Link>
+              <Nav.Link href="#pricing">About</Nav.Link>
+              <Nav.Link href="#pricing">Career</Nav.Link>
+            </Nav>
+            <Nav className="d-flex align-items-center">
+              <Nav.Link href="#deets">Profile</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+              <Button className="" variant="secondary">Login</Button>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </Container>
   );
 };
