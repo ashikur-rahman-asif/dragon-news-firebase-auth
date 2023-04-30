9b17586c0cf92e3da63f0d05f4bd4e5d51889c26
asif247 <86229363+asif247@users.noreply.github.com>
2023-05-01 04:29:19 +0600

    category added

--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
-    const [catagories, setCatagories] = useState([])
+    const [categories, setcategories] = useState([])
     useEffect(() => {
-        fetch('http://localhost:5000/catagories')
+        fetch('http://localhost:5000/categories')
             .then(res => res.json())
-            .then(data => setCatagories(data))
+            .then(data => setcategories(data))
         .catch(error=>console.log(error))
     },[])
     return (
@@ -14,7 +14,7 @@ const LeftNav = () => {
             <h4>All Category</h4>
             <div className='p-4 '>
             {
-                catagories.map(category => <p key={category.id}>
+                categories.map(category => <p key={category.id}>
                     <Link to={`/category/${category.id}`} className='text-decoration-none  text-black'>{category.name}</Link>
                 </p>)
             }
