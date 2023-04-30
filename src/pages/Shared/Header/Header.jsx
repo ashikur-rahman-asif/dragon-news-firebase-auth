1690ad38261d1eaab10c07a66a8c919994588e8a
asif247 <86229363+asif247@users.noreply.github.com>
2023-04-30 18:05:41 +0600

    react marquee (slider news) added

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
-import logo from '../../../../public/assets/logo.png'
-import moment from 'moment';
+import React from "react";
+import logo from "../../../../public/assets/logo.png";
+import moment from "moment";
+import { Button, Container } from "react-bootstrap";
+import Marquee from "react-fast-marquee";
 const Header = () => {
-    return (
-        <div>
-            <div className="text-center">
-                <img src={logo} alt="" />
-                <p className="text-secondary">
-                    <small>Journalism Without Fear or Favour</small>
-                </p>
-                <p>{moment().format("dddd, MMMM D, YYYY, h:mm:ss a")}</p>
-            </div>
-        </div>
-    );
+  return (
+    <Container className="mt-3">
+      <div className="text-center">
+        <img src={logo} alt="" />
+        <p className="text-secondary">
+          <small>Journalism Without Fear or Favour</small>
+        </p>
+        <p>{moment().format("dddd, MMMM D, YYYY, h:mm a")}</p>
+      </div>
+      <div className="d-flex">
+        <Button variant="danger">Danger</Button>
+        <Marquee speed={100}>
+          I can be a React component, multiple React components, or just some
+          text.
+        </Marquee>
+      </div>
+    </Container>
+  );
 };
 
 export default Header;
