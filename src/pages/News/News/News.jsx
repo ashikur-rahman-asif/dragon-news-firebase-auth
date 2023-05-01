4053adf5aade752b7ed4df6318cc875945690c0b
asif247 <86229363+asif247@users.noreply.github.com>
2023-05-02 01:22:43 +0600

    editors insights done

--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { Link, useLoaderData } from "react-router-dom";
+import EditorsInsights from "../EditorsInsights/EditorsInsights";
 
 const News = () => {
-    return (
-        <div>
-            <h2>News details coming son</h2>
-        </div>
-    );
+  const news = useLoaderData();
+  const { _id, title, details, image_url, author, total_view, category_id } =
+    news;
+  return (
+    <div>
+      <Card>
+        <Card.Img variant="top" src={image_url} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{details}</Card.Text>
+          <Link to={`/category/${category_id}`}>
+            <Button variant="danger">All news in this Category</Button>
+          </Link>
+        </Card.Body>
+          </Card>
+          <EditorsInsights></EditorsInsights>
+    </div>
+  );
 };
 
 export default News;
